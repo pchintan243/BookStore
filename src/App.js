b7dd0a94874c7efa8f16d64452c449e253a50c62
chintan <patelchintan843@gmail.com>
2023-05-18 19:48:42 +0530

    Day:2 router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import React from 'react'
 import './App.css';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import {
+  BrowserRouter as Router, Routes, Route
+} from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      Hello My Name is Chintan Patel and I am from Surat and currently i'm pursuing B.E Information Technology.
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/services" element={<Services />} />
+          <Route exact path="*" element={<h1>Page Not Found</h1>} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 

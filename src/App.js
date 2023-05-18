e5b2be2e21b3cf4ae3419a159098319e8124db88
chintan <patelchintan843@gmail.com>
2023-05-18 11:43:27 +0530

    Day 1: Commit the changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      hello
+      Hello My Name is Chintan Patel and I am from Surat and currently i'm pursuing B.E Information Technology.
     </div>
   );
 }

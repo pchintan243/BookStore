0a2c11c7c823158d7bbbd89c09a9314c9605f62f
chintan <patelchintan843@gmail.com>
2023-05-19 17:14:35 +0530

    carousel added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import "../App.css"
 
 const Home = () => {
   return (
     <>
-      <div className="d-flex justify-content-center align-items-center">
-        <p className='fw-bolder h1'>
-          You are in Home Page
-        </p>
+      <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={`${process.env.REACT_APP_IMG_URL3}`} className="d-block w-100 h-90" alt="lp1" style={{ height: "85vh" }} />
+          </div>
+          <div className="carousel-item">
+            <img src={`${process.env.REACT_APP_IMG_URL2}`} className="d-block w-100" alt="lp2" style={{ height: "85vh" }} />
+          </div>
+          <div className="carousel-item">
+            <img src={`${process.env.REACT_APP_IMG_URL4}`} className="d-block w-100" alt="lp3" style={{ height: "85vh" }} />
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+        </button>
       </div>
     </>
   )

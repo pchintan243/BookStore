f7b1a1209962a19b2d9ba5ab63b17fcca3b06b24
chintan <patelchintan843@gmail.com>
2023-05-22 18:21:05 +0530

    popover use from mui

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '@mui/material/Button';
+import { Popover } from '@mui/material';
+
 
 const About = () => {
-    const alertOccur = () => {
-        alert("Button Clicked..!!")
-    }
+
+    const [open, setOpen] = useState(false)
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleClick = (event) => {
+        console.log(212);
+        setAnchorEl(event.currentTarget);
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+        setOpen(false);
+    };
+
     return (
         <>
             <div className="d-flex justify-content-center align-items-center m-5">
                 <p className='fw-bolder h1 mx-3'>
                     You are in About Page
                 </p>
-                <Button variant="contained" onClick={alertOccur}>Click Me!!</Button>
+                <Button variant="contained" onClick={handleClick}>Click Me!!</Button>
             </div>
+            <Popover
+                open={open}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                }}
+                transformOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+            >
+                The content of the Popover.
+            </Popover>
+
         </>
     )
 }

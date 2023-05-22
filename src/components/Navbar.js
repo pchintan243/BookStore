1ca38b186468fac3551ed738d6bc6a2651eaad45
chintan <patelchintan843@gmail.com>
2023-05-22 16:40:27 +0530

    made navbar without bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,21 @@
 import React from 'react'
+import "./navbar.css"
 import {
   Link
 } from 'react-router-dom'
 
 const Navbar = () => {
+
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-light">
-        <div className="container-fluid">
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item fw-bold">
-                <Link className="nav-link" to="/">Home</Link>
-              </li>
-              <li className="nav-item fw-bold">
-                <Link className="nav-link" to="/about">About Us</Link>
-              </li>
-              <li className="nav-item fw-bold">
-                <Link className="nav-link" to="/contact">Contact Us</Link>
-              </li>
-              <li className="nav-item fw-bold">
-                <Link className="nav-link" to="/services">Services</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
+      <nav class="header">
+        <h1 class="logo">BookStore</h1>
+        <ul class="main-nav">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/services">Services</Link></li>
+        </ul>
       </nav>
     </>
   )

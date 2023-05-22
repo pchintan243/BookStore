a90bbfce5946f3a29b3b9d3cd7e279caedb07e42
chintan <patelchintan843@gmail.com>
2023-05-22 18:20:42 +0530

    implement usestate

--- src/components/contactPage/Contact.js
+++ src/components/contactPage/Contact.js
@@ -1,22 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./contact.css";
 
 const Contact = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [phone, setPhone] = useState()
   return (
     <>
       <div className="login-box">
         <h2>Contact Us</h2>
         <form>
           <div className="user-box">
-            <input type="text" name="" required />
+            <input type="text" name="" value={name} onChange={(e) => { setName(e.target.value) }} required />
             <label>Name</label>
           </div>
           <div className="user-box">
-            <input type="text" name="" required />
+            <input type="text" name="" value={email} onChange={(e) => { setEmail(e.target.value) }} required />
             <label>Email</label>
           </div>
           <div className="user-box">
-            <input type="text" name="" required />
+            <input type="text" name="" value={phone} onChange={(e) => { setPhone(e.target.value) }} required />
             <label>Contact Number</label>
           </div>
           <button>Submit</button>

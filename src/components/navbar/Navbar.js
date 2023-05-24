cc71d749429934538f07b466417617a259f4ade2
chintan <patelchintan843@gmail.com>
2023-05-24 16:15:45 +0530

    Day5: implementing logout functinality

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./navbar.css"
 import {
   Link
 } from 'react-router-dom'
 import { Avatar } from '@mui/material'
+import { Popover } from '@mui/material';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const Navbar = () => {
+
+  const [open, setOpen] = useState(false)
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    console.log(212);
+    setAnchorEl(event.currentTarget);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    // console.log("The new value of Name : ", name);
+    // return () => {
+    //   console.log("The old value of Name : ", name);
+    // };
+  }, []);
 
   return (
     <>
@@ -17,7 +40,35 @@ const Navbar = () => {
           <li><Link to="/contact">Contact</Link></li>
           <li><Link to="/services">Services</Link></li>
         </ul>
-        <Avatar sx={{ bgcolor: "blue", cursor: "pointer" }}>CP</Avatar>
+        <div onClick={handleClick}>
+          <Avatar sx={{ bgcolor: "blue", cursor: "pointer" }}>CP</Avatar>
+        </div>
+        <Popover
+          open={open}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+        >
+          <div style={{
+            padding: 10,
+            display: 'flex',
+            flexDirection: "column",
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+            <h6 style={{
+              fontWeight: 'bolder'
+            }}>Chintan Patel</h6>
+            <LogoutIcon style={{ cursor: "pointer" }} />
+          </div>
+        </Popover>
       </nav>
     </>
   )

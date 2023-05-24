357988233085a1b6a94cd0b5842be9209f068391
chintan <patelchintan843@gmail.com>
2023-05-24 16:16:09 +0530

    remove pop up and usestate instead of navbar

--- src/components/About.js
+++ src/components/About.js
@@ -1,24 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Button from '@mui/material/Button';
-import { Popover } from '@mui/material';
-
 
 const About = () => {
 
-    const [open, setOpen] = useState(false)
-    const [anchorEl, setAnchorEl] = useState(null);
+    const handleClick = () => {
 
-    const handleClick = (event) => {
-        console.log(212);
-        setAnchorEl(event.currentTarget);
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setAnchorEl(null);
-        setOpen(false);
-    };
-
+    }
     return (
         <>
             <div className="d-flex justify-content-center align-items-center m-5">
@@ -27,22 +14,6 @@ const About = () => {
                 </p>
                 <Button variant="contained" onClick={handleClick}>Click Me!!</Button>
             </div>
-            <Popover
-                open={open}
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'right',
-                }}
-                transformOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                }}
-                anchorEl={anchorEl}
-                onClose={handleClose}
-            >
-                The content of the Popover.
-            </Popover>
-
         </>
     )
 }

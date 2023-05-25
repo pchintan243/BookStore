e5afe818a0bf08240a75ca19781cc3d08a3e4da7
chintan <patelchintan843@gmail.com>
2023-05-25 17:39:20 +0530

    login component

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -36,6 +36,7 @@ const Navbar = () => {
           <li><Link to="/about">About</Link></li>
           <li><Link to="/contact">Contact</Link></li>
           <li><Link to="/services">Services</Link></li>
+          <li><Link to="/login">Login</Link></li>
         </ul>
         <div onClick={handleClick}>
           <Avatar sx={{ bgcolor: "blue", cursor: "pointer" }}>CP</Avatar>

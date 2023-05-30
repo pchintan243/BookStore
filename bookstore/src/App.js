7fa3d4378789c235cc8ae3bcafcdf6bdc2b11f50
chintan <patelchintan843@gmail.com>
2023-05-30 13:26:45 +0530

    modify it

--- bookstore/src/App.js
+++ bookstore/src/App.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 import Navbar from './components/navbar/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Services from './components/Services';
+import Home from './components/home/Home';
 import {
   BrowserRouter as Router, Routes, Route, Navigate
 } from 'react-router-dom'
@@ -25,8 +23,6 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/BookStore" element={<Navigate to="/" />}></Route>
-            <Route exact path="/about" element={<About />} />
-            <Route exact path="/services" element={<Services />} />
             <Route exact path="/signup" element={<Signup />} />
             <Route exact path="/login" element={<Login />} />
             <Route exact path="/*" element={<h1>Page Not Found</h1>} />

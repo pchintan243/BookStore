025f843dbf7670a88cb18860dd4da89ed91cb3fd
chintan <patelchintan843@gmail.com>
2023-05-30 16:38:14 +0530

    navbar completed

--- bookstore/src/components/navbar/Navbar.js
+++ bookstore/src/components/navbar/Navbar.js
@@ -3,18 +3,51 @@ import "./navbar.css"
 import { Link } from 'react-router-dom'
 import { ShoppingCart } from '@mui/icons-material';
 import loginContext from '../../context/loginContext';
+import { toast } from 'react-toastify';
+import { Button } from '@mui/material';
 
 const Navbar = () => {
   const isLogin = useContext(loginContext)
   const valueChange = () => {
     isLogin.setLogin(false)
+    toast.success('Logout Succesfully..!!', {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
   }
 
   const NavChange = () => {
     // If user is logged in then it shows Logout button, otherwise it shows register or login button
     if (isLogin.login) {
       return (
-        <li><Link to="/login" className='linkTag color-red' onClick={valueChange}>Logout</Link></li>
+        <>
+          <li>
+            <Link to="/update" className='linkTag color-red' style={{
+              margin: 4,
+              fontWeight: "600",
+            }}
+            >
+              Update Profile
+            </Link>
+          </li>
+          <Button variant="outlined" onClick={valueChange} style={{
+            border: " 1px solid black",
+            color: "red",
+            width: "79px",
+            fontSize: "13px",
+            background: "#ff00000f",
+            fontWeight: "600",
+          }}
+          >
+            Logout
+          </Button>
+        </>
       )
     }
     else {
@@ -38,7 +71,7 @@ const Navbar = () => {
           <NavChange />
 
           <li className='cart-li'>
-            <Link to="/cart" className='color-red'>
+            <Link to="/cart" className='color-red' style={{ background: "#ff00000f" }} >
               <ShoppingCart className='color-red' />
               <span>0</span>
               <span className='cartTag'>Cart</span>
